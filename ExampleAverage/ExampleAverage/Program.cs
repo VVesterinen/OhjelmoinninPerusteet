2ad2d5e642140b9ade1f2d502597867ef3e36821
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-09-17 11:54:36 +0300

    fix

---
+++ ExampleAverage/ExampleAverage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            int i = 1;
+            Console.Write("Ohjelma laskee annettujen lukujen keskiarvon. " +
+                "Kuinka monta lukua syötät? ");
+            int lkm = int.Parse(Console.ReadLine());
+
+            while(i<=lkm)
+
+            {
+                int x = int.Parse(Console.ReadLine());
+                summa = summa + x;
+                i = i + 1;
+            }
+
+            if(lkm > 0)
+
+            {
+                double ka = summa / lkm;
+                Console.WriteLine("Keskiarvo on: " + ka);
+            }
+            else
+            {
+                Console.WriteLine("Lukuja ei ole syötetty!");
+            }
+
+            Console.WriteLine(lkm);
+        }
+    }
+}
