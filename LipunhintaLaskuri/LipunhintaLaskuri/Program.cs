fa5226b4ed4ecd2c66c412b3aca7c7b55432b913
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-09-18 20:13:03 +0300

    sume

---
+++ LipunhintaLaskuri/LipunhintaLaskuri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LipunhintaLaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
