b1db1fb72630ca0317df7b5e7046e7fd0a32b48a
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-09-18 15:22:52 +0300

    fix

--- ProjektKertoma/ProjektKertoma/Program.cs
+++ ProjektKertoma/ProjektKertoma/Program.cs
@@ -6,7 +6,29 @@ namespace ProjektKertoma
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int fact = 1;
+
             Console.WriteLine("Kertoman laskenta");
+            Console.Write("Minkä luvun kertoma lasketaan? ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+
+            else
+
+            {
+                while(n > 0)
+                {
+                    fact = fact * n;
+                    n = n - 1;
+                }
+                Console.WriteLine("Vastaus: " + fact);
+            }
         }
     }
 }
+
