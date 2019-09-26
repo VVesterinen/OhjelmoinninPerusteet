b8750b83723aa62f7b18f9c2c87aadee8f6e1dd7
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-09-26 13:37:22 +0300

    new

---
+++ ReadWriteExample/ReadWriteExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReadWriteExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ReadWriteExample");
+            Console.Write("Your name; ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("Excuse me " + userInput + ", can you hear me?");
+            Console.WriteLine("Excuse me {0}, can you hear me?", userInput);
+            Console.WriteLine($"Excuse me {userInput}, can you hear me?");
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
