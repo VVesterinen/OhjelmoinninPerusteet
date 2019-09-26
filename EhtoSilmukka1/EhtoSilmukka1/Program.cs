8f994e927bf4119914d1ab89fd0feb28aaab94b1
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-09-26 13:13:24 +0300

    new

---
+++ EhtoSilmukka1/EhtoSilmukka1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EhtoSilmukka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            int n = 0;
+
+
+
+            Console.WriteLine("Nega, Posi vai Nolla?");
+            n = int.Parse(Console.ReadLine());
+
+
+            if (n < 0)
+            {
+                Console.WriteLine("Luku on negatiivinen.");
+            }
+
+            else if (n > 0)
+
+            {
+                Console.WriteLine("Luku on positiivinen.");
+            }
+
+            else if (n == 0)
+
+            {
+                Console.WriteLine("Luku on nolla.");
+            }
+
+        }
+    }
+}
