b29febabdf79bb5cad3a9356bdac34f9f406c4f9
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-10-03 08:39:00 +0300

    fix

--- LipunhintaLaskuri/LipunhintaLaskuri/Program.cs
+++ LipunhintaLaskuri/LipunhintaLaskuri/Program.cs
@@ -12,29 +12,39 @@ namespace LipunhintaLaskuri
             string varusmies;
             string opiskelija;
             string mtkjäsen;
-            int age;
+            int age = 0;
+
+
 
             Console.WriteLine("Pääsylippu hinnan laskuri");
             Console.Write("Oletko varusmies? Yes / No: ");
             varusmies = Console.ReadLine();
 
-            if (varusmies.ToUpper() == "Yes");
+            if (varusmies.ToUpper() == "Yes") ;
 
             {
                 discount = 0.5;
                 Console.WriteLine($"Lipun hinta on {originalPrice - (discount * originalPrice)} €");
             }
 
-            else
+              else if (varusmies.ToUpper() == "No") ;
 
             {
                 Console.Write("Oletko opiskelija? Yes / No: ");
                 opiskelija = Console.ReadLine();
+                Console.Write("Oletko Mtk:n jäsen? Yes / No: ");
+                mtkjäsen = Console.ReadLine();
 
-                    if (opiskelija.ToUpper() == "Yes") ;
+                if (opiskelija.ToUpper() == "Yes" && mtkjäsen.ToUpper() == "Yes") ;
+                {
+                    discount = 0.60;
+                    Console.WriteLine($"Lipun hinta on {originalPrice - (discount * originalPrice)} €");
+                }
+
+                if (opiskelija.ToUpper() == "Yes" && mtkjäsen.ToUpper() == "No") ;
                 {
                     discount = 0.45;
-                    
+                    Console.WriteLine($"Lipun hinta on {originalPrice - (discount * originalPrice)} €");
                 }
 
 
