9b0cf52bdf07758fcd0e3ab84d4313224d429671
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-10-10 10:38:03 +0300

    fix

---
+++ ProjektKertoma/ProjektiYhteenlasku/ProjektiYhteenlasku/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProjektiYhteenlasku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int luku;
+            int vastaus = 0;
+            int i = 0; 
+
+
+            Console.WriteLine("Yhteenlasku laskuri");
+            Console.Write("Syötä luku: ");
+            luku = int.Parse(Console.ReadLine());
+
+            if (luku >= 1)
+            {
+                while (i <= luku)
+                {
+                    vastaus = (vastaus + i);
+                    i++;
+                }
+                Console.WriteLine("Vastaus on: " + vastaus);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+
+        }
+    }
+}
