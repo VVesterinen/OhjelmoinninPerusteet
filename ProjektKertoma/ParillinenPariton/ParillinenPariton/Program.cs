22d7d91edca0c4467b663e43b16d809c6dc1b068
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-10-17 14:37:34 +0300

    yolo

---
+++ ProjektKertoma/ParillinenPariton/ParillinenPariton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ParillinenPariton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Syötä luku: ");
+            int luku = int.Parse(Console.ReadLine());
+
+            int i = 
+
+
+           
+
+
+
+
+
+
+
+        }
+    }
+}
