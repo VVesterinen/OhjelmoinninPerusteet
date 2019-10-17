57c4264fcbe13f070fd7999988be83767610597e
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-10-17 15:07:01 +0300

    Update Program.cs

--- ProjektKertoma/ParillinenPariton/ParillinenPariton/Program.cs
+++ ProjektKertoma/ParillinenPariton/ParillinenPariton/Program.cs
@@ -7,19 +7,39 @@ namespace ParillinenPariton
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Parillisten ja parittomien lukujen summien yhteenlasku kone.");
             Console.Write("Syötä luku: ");
             int luku = int.Parse(Console.ReadLine());
 
-            int i = 
+            int i = 1;
+            int pariton = 0;
+            int parillinen = 0;
 
+            if (luku >= 1)
+            {
+                while (i <= luku)
+                {
+                    if (i % 2 == 0)
+                    {
+                        parillinen = parillinen + i;
+                    }
 
-           
+                    else
+                    {
+                        pariton = pariton + i;
+                    }
+                    i++;
+                }
 
+                Console.WriteLine("Vastaus: Pariton summa = " + pariton + "," +
+                    " Parillisten summa = " + parillinen + ".");
 
+            }
 
-
-
-
+           else
+            {
+                Console.WriteLine("Luku on liian pieni");
+            }
 
         }
     }
