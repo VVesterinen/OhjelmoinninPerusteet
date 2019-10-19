463b5ece20e910d31770a5def5d6a2098908fe47
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-10-19 12:15:56 +0300

    new

---
+++ EhtoSilmukka1/EhtoSilmukka2.1/EhtoSilmukka2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EhtoSilmukka2._1
+{
+        class Program
+        {
+            static void Main(string[] args)
+
+            {
+                Console.WriteLine("Syötä luku");
+                Console.Write("");
+
+                int lkm = int.Parse(Console.ReadLine());
+
+                if (lkm % 2 == 0)
+                {
+                    Console.WriteLine("Luku on parillinen.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Luku on pariton.");
+                }
+
+            }
+        }
+    }
