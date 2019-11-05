33310acb32b20cf876cd8d63c51890a7ad1a4507
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-05 10:17:50 +0200

    new

--- Toistorakenne/ToistorakenneJatkuu4/ToistorakenneJatkuu4/Program.cs
+++ Toistorakenne/ToistorakenneJatkuu4/ToistorakenneJatkuu4/Program.cs
@@ -7,20 +7,33 @@ namespace ToistorakenneJatkuu4
         static void Main(string[] args)
         {
 
+            Random rnd = new Random();
+            int x;
+            int y = 0;
 
+            int Kruuna = 0, Klaava = 0;
 
+            Console.Write("Monta kertaa kolikkoa heitetään? ");
+            x = int.Parse(Console.ReadLine());
 
+            for (y = 0; y < x;y++)
+            {
+                int result = rnd.Next(2);
 
+                if (result == 1)
+                {
+                    Kruuna++;
+                }
 
+                else if (result == 0)
+                {
+                    Klaava++;
+                }
+            }
 
-
-
-
-
-
-
-
-
+            Console.WriteLine("Kolikkoa on heitetty " + x + " kertaa.");
+            Console.WriteLine("Klaavoja oli " + Klaava + " ja kruunuja " + Kruuna + ".");
+            return;
         }
     }
 }
