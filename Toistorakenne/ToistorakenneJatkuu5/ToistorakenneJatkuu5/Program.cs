29134a1734ae2f484c6946107201ec2e3530e14e
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 09:53:11 +0200

    Update Program.cs

--- Toistorakenne/ToistorakenneJatkuu5/ToistorakenneJatkuu5/Program.cs
+++ Toistorakenne/ToistorakenneJatkuu5/ToistorakenneJatkuu5/Program.cs
@@ -6,14 +6,30 @@ namespace ToistorakenneJatkuu5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pitkäveto lappusi:");
+            Console.WriteLine();
 
+            Random rnd = new Random();
 
+            for (int n = 1; n < 14; n++)
+            {
+                double result = rnd.NextDouble();
 
+                if (result <= 0.4)
+                {
+                    Console.WriteLine("{0}. 1", n);
+                }
 
+                else if (result > 0.4 && result <= 0.6)
+                {
+                    Console.WriteLine("{0}. X", n);
+                }
 
-
-
-
+                else
+                {
+                    Console.WriteLine("{0}. 2", n);
+                }
+            }
         }
     }
 }
