3d9e8f7b7a21fa0175b7980d236873ad0505e020
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 10:11:15 +0200

    Update Program.cs

--- Toistorakenne/ToistorakenneJatkuu6/ToistorakenneJatkuu6/Program.cs
+++ Toistorakenne/ToistorakenneJatkuu6/ToistorakenneJatkuu6/Program.cs
@@ -7,14 +7,26 @@ namespace ToistorakenneJatkuu6
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Dice throw generator.");
+            Console.WriteLine();
 
+            Random rnd = new Random();
+            int six = 0;
 
+            for (int n = 1; n <= 1000; n++)
+            {
+                int result = rnd.Next(6);
+                Console.WriteLine($"{n}. {result + 1}");
 
+                if (result + 1 == 6)
+                {
+                    six++;
+                }
+            }
 
-
-
-
-
+            Console.WriteLine();
+            Console.WriteLine($"Six was thrown {six} times.");
+            return;
         }
     }
 }
