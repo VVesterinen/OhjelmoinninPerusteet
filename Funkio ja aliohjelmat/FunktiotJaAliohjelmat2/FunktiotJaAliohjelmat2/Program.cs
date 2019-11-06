833a996b123aec28d6da62041f3714a2183e982a
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 11:29:39 +0200

    Update Program.cs

--- Funkio ja aliohjelmat/FunktiotJaAliohjelmat2/FunktiotJaAliohjelmat2/Program.cs
+++ Funkio ja aliohjelmat/FunktiotJaAliohjelmat2/FunktiotJaAliohjelmat2/Program.cs
@@ -6,7 +6,41 @@ namespace FunktiotJaAliohjelmat2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input two numbers: ");
+            Console.WriteLine();
+
+            Console.Write("Input 1. number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Input 2. number: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine(GetMinNumber(x, y));
+
         }
+
+        static string GetMinNumber(int x, int y)
+        {
+
+            string text = "";
+
+            if (x < y)
+            {
+                Console.WriteLine($"Number {x} is smaller than number {y}.");
+            }
+
+            else if (x == y)
+            {
+                Console.WriteLine("Numbers are the same.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Number {y} is smaller than number {x}.");
+            }
+
+            return text;
+
+        } 
     }
 }
