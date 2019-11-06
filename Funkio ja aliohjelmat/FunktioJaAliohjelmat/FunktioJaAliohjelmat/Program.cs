49426d9a2861f7d26824afb8abb6caf640373bee
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 11:08:12 +0200

    Update Program.cs

--- Funkio ja aliohjelmat/FunktioJaAliohjelmat/FunktioJaAliohjelmat/Program.cs
+++ Funkio ja aliohjelmat/FunktioJaAliohjelmat/FunktioJaAliohjelmat/Program.cs
@@ -6,7 +6,36 @@ namespace FunktioJaAliohjelmat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region "Number input"
+
+            Console.WriteLine("How many stars? ");
+            int i = int.Parse(Console.ReadLine());
+
+            if (i < 1)
+            {
+                Console.WriteLine($"Number {i} isn't allowed.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{Change(i)}");
+            }
+            #endregion
         }
+        #region "Number to Star"
+
+        static string Change(int n)
+
+        {
+            string value = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                value += "*";
+            }
+            return value; 
+
+        }
+        #endregion
     }
 }
