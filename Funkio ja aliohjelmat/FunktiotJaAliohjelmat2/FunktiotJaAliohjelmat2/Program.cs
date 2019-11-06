5605c8b955d184f441f5a8fccc057dfa0eaa9af6
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 10:50:37 +0200

    new

---
+++ Funkio ja aliohjelmat/FunktiotJaAliohjelmat2/FunktiotJaAliohjelmat2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunktiotJaAliohjelmat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
