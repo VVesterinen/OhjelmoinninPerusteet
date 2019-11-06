fd9fa3cf9e1c042596c52d906d6b9efe18b5ece8
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-06 09:35:57 +0200

    new

---
+++ Toistorakenne/ToistorakenneJatkuu5/ToistorakenneJatkuu5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToistorakenneJatkuu5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
