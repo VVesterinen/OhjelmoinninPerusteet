b4acef9177cfc6721c99718ac99c8feb20c1483f
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-09 15:15:29 +0200

    aivansama

--- Funkio ja aliohjelmat/FunktioJaAliohjelmat3/FunktiotJaAliohjelmat3/Program.cs
+++ Funkio ja aliohjelmat/FunktioJaAliohjelmat3/FunktiotJaAliohjelmat3/Program.cs
@@ -6,7 +6,45 @@ namespace FunktiotJaAliohjelmat3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool isValid = false;
+
+            while (isValid == false)
+            {
+                Console.WriteLine("Give a number: ");
+                String userInput = Console.ReadLine();
+
+                isValid = numberFromRange(userInput, 1, 50);
+            }
+
+            if (isValid == true)
+            {
+                Console.WriteLine($"Your input {userInput} passes test.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Your input {userInput} is out of range.");
+            }
+
         }
+
+        #region "Solve"
+        static bool numberFromRange(String userValue, int lowerBound, int upperBound)
+
+        {
+
+            bool isInRange = false;
+            try
+            {
+
+                if (userValue >= lowerBound && userValue <= upperBound)
+                {
+                    isInRange = true;
+                }
+            }
+            return isInRange;
+        }
+        #endregion
     }
 }
