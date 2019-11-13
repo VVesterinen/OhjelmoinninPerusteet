8ff235f97e1630266dfc16a0ea8e6ad74f0ea226
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-13 08:51:37 +0200

    Update Program.cs

--- Taulukot/Taulukot/Taulukot/Program.cs
+++ Taulukot/Taulukot/Taulukot/Program.cs
@@ -3,19 +3,28 @@
 namespace Taulukot
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            Console.WriteLine("Taulukko esimerkki");
 
+            //luodaan numbers taulukko.
+            int[] numbers = new int[] { 1, 3, 5, 7, 9 };
 
+            //asetetaan arvo numbers taulukkoon
+            numbers[4] = 11;
 
+            
+            //luetaan arvo taulukosta
+            Console.WriteLine(numbers[2]);
+            
 
-
-
-
-
-
-
+            //luetaan arvot silmukassa
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
+        
     }
 }
