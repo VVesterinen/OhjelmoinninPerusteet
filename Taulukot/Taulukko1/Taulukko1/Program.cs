0ca048a6419e7c2cd97f90dcfa398331b32565ef
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-11-15 09:13:37 +0200

    New and finnished task

---
+++ Taulukot/Taulukko1/Taulukko1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Taulukko1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region "Task1"
+            double[] productPrice = { 7.92, 72.0, 99.90  };
+            int[] productAmount = { 1, 3, 2 };
+            double[] productTotalPrice = { (productPrice[0] * productAmount[0]), (productPrice[1] * productAmount[1]), (productPrice[2] * productAmount[2]) };
+
+            Console.WriteLine($"Product 1: {productTotalPrice[0]}€");
+            Console.WriteLine($"Product 2: {productTotalPrice[1]}€");
+            Console.WriteLine($"Product 3: {productTotalPrice[2]}€");
+            #endregion
+            Console.WriteLine("");
+            #region "Task2"
+            double sum = 0;
+            int n = 0;
+            int[] iT = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                iT[n] = rnd.Next(50);
+                n++;
+            }
+            n = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                sum += iT[n];
+                Console.WriteLine($"{n + 1}. {iT[n]}");
+                n++;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"The average is {sum / iT.Length}.");
+            #endregion
+        }
+    }
+}
