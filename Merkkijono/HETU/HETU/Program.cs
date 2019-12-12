50623c840443026b3fd6445216f0989b4721b529
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2019-12-12 12:54:57 +0200

    new program

---
+++ Merkkijono/HETU/HETU/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HETU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will ask your social-ID and will check it, " +
+                "if its correct.");
+            
+        }
+    }
+}
