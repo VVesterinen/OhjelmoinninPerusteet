527230edf5f0b8888fa30f548e7f61300fecb1ba
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2020-01-28 16:46:04 +0200

    new program

---
+++ Merkkijono/BBAN-Validator/BBAN-Validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BBAN_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
