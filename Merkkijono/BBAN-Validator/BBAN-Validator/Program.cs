7eff38408d34552e4cb82cc59a0536119fbb48db
Veeti Vesterinen <veeti.vesterinen@finnmareka.fi>
2020-01-28 17:16:19 +0200

    added main funtion, added null funtion and validation funktion

--- Merkkijono/BBAN-Validator/BBAN-Validator/Program.cs
+++ Merkkijono/BBAN-Validator/BBAN-Validator/Program.cs
@@ -6,7 +6,69 @@ namespace BBAN_Validator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give a BBAN:\n\n");
+            String userInput = Console.ReadLine();
+            userInput = userInput.Replace("-", "");
+            int i = 0;
+            Bank(userInput, ref i);
+            PlusZero(ref userInput, i);
+
+            if (Check(userInput))
+                Console.WriteLine("This is a valid BBAN");
+            else
+                Console.WriteLine("This is not a valid BBAN");
+        }
+
+        static void Bank(String userInput, ref int i)
+        {
+            char firstNumber = userInput[0];
+            if (firstNumber == '4' || firstNumber == '5' || firstNumber == '7')
+            {
+                i = 1;
+            }
+            else
+            {
+                i = 2;
+            }
+        }
+
+        static void PlusZero(ref String userInput, int i)
+        {
+            if (i == 1)
+            {
+                while (userInput.Length < 14)
+                {
+                    userInput = userInput.Insert(7, "0");
+                }
+            }
+            else
+            {
+                while (userInput.Length < 14)
+                {
+                    userInput = userInput.Insert(6, "0");
+                }
+            }
+        }
+
+        static bool Check(String userInputValue)
+        {
+            int Digits = userInputValue.Length;
+
+            int Sum = 0;
+            bool second = false;
+            for (int i = Digits - 1; i >= 0; i--)
+            {
+                int n = userInputValue[i] - '0';
+
+                if (second == true)
+                    n = n * 2;
+
+                Sum += n / 10;
+                Sum += n % 10;
+
+                second = !second;
+            }
+            return (Sum % 10 == 0);
         }
     }
 }
